691e8f29e83a37eb2411ef9421118c490a605a34
Gianni Moschini <gianni.moschini+github@gmail.com>
2020-09-01 11:18:24 +0100

    Fix JS code

--- src/gatsby-ssr.js
+++ src/gatsby-ssr.js
@@ -1,20 +1,24 @@
 import React from "react";
 
 exports.onRenderBody = ({ setPostBodyComponents }, pluginOptions) => {
-  if (process.env.NODE_ENV === `production`) {
-    const { id } = pluginOptions;
-    return setPostBodyComponents([
-    <script
-      key={`gatsby-plugin-lead-forensics`}
-      dangerouslySetInnerHTML={{
-        __html: `
-  <script type="text/javascript" src="https://secure.leadforensics.com/js/${id}.js" ></script>
-  <noscript><img alt="" src="https://secure.leadforensics.com/${id}.png" style="display:none;" /></noscript>
-          `
-        }}
-      />
-      ]);
-    }
+  //if (process.env.NODE_ENV === `production`) {
+  const { id } = pluginOptions;
+  return setPostBodyComponents([
+    <>
+      <script
+        type="text/javascript"
+        src={`https://secure.leadforensics.com/js/${id}.js`}
+      ></script>
+      <noscript>
+        <img
+          alt=""
+          src={`https://secure.leadforensics.com/${id}.png`}
+          style={{ display: "none" }}
+        />
+      </noscript>
+    </>
+  ]);
+  // }
 
   return null;
 };
