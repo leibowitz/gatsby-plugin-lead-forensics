7af961a6418582d826b53131721b9323573d023e
Gianni Moschini <gianni.moschini+github@gmail.com>
2020-09-01 08:56:22 +0100

    Add initial code for plugin

---
+++ src/gatsby-ssr.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+exports.onRenderBody = ({ setPostBodyComponents }, pluginOptions) => {
+  if (process.env.NODE_ENV === `production`) {
+    const { id } = pluginOptions;
+    return setPostBodyComponents([
+    <script
+      key={`gatsby-plugin-lead-forensics`}
+      dangerouslySetInnerHTML={{
+        __html: `
+  <script type="text/javascript" src="https://secure.leadforensics.com/js/${id}.js" ></script>
+  <noscript><img alt="" src="https://secure.leadforensics.com/${id}.png" style="display:none;" /></noscript>
+          `
+        }}
+      />
+      ]);
+    }
+
+  return null;
+};
